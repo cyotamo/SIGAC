f4ce05461e61a8eccd45d261478ea849c98c5219
cyotamo <cyotamo@yahoo.com.br>
2026-02-14 19:12:28 +0200

    Update faculdades.js

--- faculdades.js
+++ faculdades.js
@@ -1,2 +1,2 @@
-export const WEB_URL = "https://script.google.com/macros/s/AKfycbz2uYJ7708eNcCnHonI6i0a0YQA2GiyrVId6hOO4lwuQiFyg9nlLH4FSUF7uqQNErnt_Q/exec";
+export const WEB_URL = "https://script.google.com/macros/s/AKfycbyAz_m2w8vmzB2sEbND6l2kWgSE50qmn70cizqe_s8ZhSgmxZyVzbuqLoxAC-Qrn-Xfdg/exec";
 
